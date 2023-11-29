99366dd5b71b45250dfa12bb58986062ef306ada
Eya Gargah <eyagargah@hotmail.com>
2023-11-30 00:04:41 +0100

    toggle dark theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quiz-app';
+
+  toggleDarkTheme(): void {
+    document.body.classList.toggle('dark-theme');
+  }
 }
