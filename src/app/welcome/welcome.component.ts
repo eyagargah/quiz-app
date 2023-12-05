25f3822c344394bcd5fdebf19ab738570bf6ace9
Eya Gargah <eyagargah@hotmail.com>
2023-12-05 01:02:43 +0100

    Update welcome.component.ts

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import  quizzesData from '../../assets/quizes.json';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
@@ -12,9 +13,13 @@ export class WelcomeComponent {
     console.log(this.data[0].title)
    }
 
+
+   constructor(private router:Router){}
+
    getQuizQuestions(e:any , i:any){
     
     this.questions = this.data[i].questions;
+    this.router.navigateByUrl('quiz');
    }
 
 }
