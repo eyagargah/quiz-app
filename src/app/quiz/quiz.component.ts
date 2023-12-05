9bde40e15511d4c803b442a6369e3ebc509d8d15
Eya Gargah <eyagargah@hotmail.com>
2023-12-05 22:14:24 +0100

    Update quiz.component.ts

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { QuizService } from 'app/quiz.service';
 
 @Component({
   selector: 'app-quiz',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class QuizComponent {
 
+  title : any;
+  questions : any;
+  icon : any;
+constructor(private quizService : QuizService){}
+
+ngOnInit(){
+  this.title = this.quizService.getTitle()
+  this.icon = this.quizService.getIcon()
+  this.questions = this.quizService.getSubjectQuestions()
+  console.log(this.title)
+  console.log(this.icon)
+  console.table(this.questions)
+}
 }
