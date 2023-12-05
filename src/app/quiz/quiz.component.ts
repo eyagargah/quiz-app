6b45f2d0ab06b145f57a639802f54a8e347d0b52
Eya Gargah <eyagargah@hotmail.com>
2023-12-06 00:08:39 +0100

    Update quiz.component.ts

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { QuizService } from 'app/quiz.service';
 
 @Component({
@@ -11,14 +12,26 @@ export class QuizComponent {
   title : any;
   questions : any;
   icon : any;
-constructor(private quizService : QuizService){}
+  next = false ;
+  i=0
+  length : any
+constructor(private quizService : QuizService , private router:Router){}
 
 ngOnInit(){
   this.title = this.quizService.getTitle()
   this.icon = this.quizService.getIcon()
   this.questions = this.quizService.getSubjectQuestions()
+  
   console.log(this.title)
   console.log(this.icon)
   console.table(this.questions)
+}
+
+submitAnswer(){
+  if(this.i<this.questions.length){
+    this.i++
+  }else {
+    this.router.navigate(['score']);
+  }
 }
 }
