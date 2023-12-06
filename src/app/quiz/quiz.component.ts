fa07bbc01756adadb9f71d3b699836d55bb6b8a3
Eya Gargah <eyagargah@hotmail.com>
2023-12-06 21:33:24 +0100

    Update quiz.component.ts

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -8,30 +8,34 @@ import { QuizService } from 'app/quiz.service';
   styleUrls: ['./quiz.component.scss']
 })
 export class QuizComponent {
-
   title : any;
   questions : any;
   icon : any;
   next = false ;
   i=0
   length : any
+  selectedAnswer : any;
+  score: any;
 constructor(private quizService : QuizService , private router:Router){}
 
 ngOnInit(){
   this.title = this.quizService.getTitle()
   this.icon = this.quizService.getIcon()
   this.questions = this.quizService.getSubjectQuestions()
-  
   console.log(this.title)
   console.log(this.icon)
   console.table(this.questions)
 }
 
 submitAnswer(){
-  if(this.i<this.questions.length){
+  if(this.i<9){
     this.i++
-  }else {
+  }else if(this.i==9){
     this.router.navigate(['score']);
   }
+}
+
+calculateScore(){
+  
 }
 }
