9cbdb9ff8d887de153366d27016aed954f943af9
Eya Gargah <eyagargah@hotmail.com>
2023-12-06 21:33:27 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quiz-app';
-
+ 
   toggleDarkTheme(): void {
     document.body.classList.toggle('dark-theme');
   }
