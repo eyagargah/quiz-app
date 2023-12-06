f070b6e455fc58e3d9fa3f883322167b0e4aacf1
Eya Gargah <eyagargah@hotmail.com>
2023-12-06 21:32:57 +0100

    fix dark mode button backgroud color

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { QuizComponent } from './quiz/quiz.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { ScoreComponent } from './score/score.component';
 
 const routes: Routes = [
   {path : '' , component: WelcomeComponent},
   {path : 'quiz' , component: QuizComponent},
-  {path : 'score' , component: QuizComponent},
+  {path : 'score' , component: ScoreComponent},
 ]
 
 @NgModule({
