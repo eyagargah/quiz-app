192dc07885a33ad2ed59381d7a85368d5d76997b
Eya Gargah <eyagargah@hotmail.com>
2023-12-11 22:37:36 +0100

    Update quiz.component.ts

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,79 +1,71 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { QuizService } from 'app/quiz.service';
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
-  styleUrls: ['./quiz.component.scss']
+  styleUrls: ['./quiz.component.scss'],
 })
 export class QuizComponent {
-  title : any;
+  title: any;
   questionsData: any;
-  questions : any;
-  icon : any;
-  next = false ;
-  i=0
-  length : any
-  score= 0;
-  constructor(private quizService : QuizService , private router:Router){}
+  questions: any;
+  icon: any;
+  next = true;
+  i = 0;
+  index: any;
+  score = 0;
+  constructor(private router: Router) {}
 
-ngOnInit(){
-  
-  this.questions = JSON.parse(localStorage.getItem('questions')!)
-  console.log(this.questions)
-  var buttons = document.querySelectorAll('.option');
-  var submitBtn = document.querySelector('.submit')
-  submitBtn?.setAttribute('disabled','')
-    // Add click event listener to each button
-  buttons.forEach(function(button) {
-    button.addEventListener('click', function(e:any) {
-      submitBtn?.removeAttribute('disabled')
-      // Remove active class from all buttons
-      buttons.forEach(function(btn) {
-        btn.classList.remove('active');
-        console.log(btn)
+  ngOnInit() {
+    this.questionsData = JSON.parse(localStorage.getItem('questions')!);
+    this.questions = this.questionsData[this.i];
+    var buttons = document.querySelectorAll('.option');
+    var submitBtn = document.querySelector('.submitBtn');
+    buttons.forEach(function (button) {
+      button.addEventListener('click', function (e: any) {
+        submitBtn?.removeAttribute('disabled');
+        if (submitBtn) {
+          submitBtn.innerHTML = 'Next Question';
+        }
+        buttons.forEach(function (btn) {
+          btn.classList.remove('active');
+        });
+        let selectedAnswer = e.target.parentNode;
+        selectedAnswer.classList.add('active');
       });
-      // Add active class to the clicked button
-      let selectedAnswer = e.target.parentNode
-      selectedAnswer.classList.add('active');
     });
-  });
-}
+  }
 
-submitAnswer(i:any){
-  var options = document.querySelector('.options')
+  submitAnswer() {
+    let selectedAnswer = document.querySelector('.active');
+    let correctAnswerIndex;
+    for (let j = 0; j < this.questions.options.length; j++) {
+      if (this.questions.answer == this.questions.options[j]) {
+        correctAnswerIndex = j;
+      }
+    }
 
-  let answer = document.querySelector('.active')?.children[1].innerHTML
-  let submitBtn = document.querySelector('.submit')
-  let correctAnswerIndex ;
-  console.table(this.questions[i].options)
+    if (
+      correctAnswerIndex &&
+      selectedAnswer?.children[1].innerHTML ==
+        this.questions.options[correctAnswerIndex!]
+    ) {
+      selectedAnswer?.classList.add('correct');
+    } else {
+      selectedAnswer?.classList.add('wrong');
+      if (correctAnswerIndex) {
+        document
+          .querySelector('.options')
+          ?.children[correctAnswerIndex].classList.add('correct');
+      }
+    }
 
-  //optimize this section
-  for(let j=0 ; j<this.questions[i].options.length ; j++){
-    if(this.questions[i].options[j]== this.questions[i].answer){
-      correctAnswerIndex = j 
+    if (this.i < 10 ) {
+      this.i += 1;
+      this.questions = this.questionsData[this.i];
+    } else if (this.i == 10) {
+      this.router.navigate(['score']);
     }
   }
-  console.log(correctAnswerIndex)
-
-if(submitBtn){
-  submitBtn.innerHTML = "Next Question"
-
-}  let selectedAnswerBtn = document.querySelector('.active')
-  if(answer == this.questions[i].answer ){
-    selectedAnswerBtn?.classList.remove("active")
-    selectedAnswerBtn?.classList.add('correct')
-    this.score += 1
-  }else {
-    selectedAnswerBtn?.classList.remove("active")
-    selectedAnswerBtn?.classList.add('wrong')
-    if(options && correctAnswerIndex){
-      options.children[correctAnswerIndex].classList.add("correct")
-    }
-  }
-  
- i++
-}
-
 }
