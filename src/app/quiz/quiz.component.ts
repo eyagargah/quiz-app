03f184b08848d028f85c5448311e50d6dee83a7b
Eya Gargah <eyagargah@hotmail.com>
2023-12-14 21:57:43 +0100

    Update quiz.component.ts

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -7,6 +7,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./quiz.component.scss'],
 })
 export class QuizComponent {
+  options = document.querySelectorAll('.options');
+  submitBtn = document.querySelector('.submitBtn');
   title: any;
   questionsData: any;
   questions: any;
@@ -15,64 +17,56 @@ export class QuizComponent {
   i = 0;
   index: any;
   score = 0;
+  correctAnswerIndex: any;
+
   constructor(private router: Router) {}
 
   ngOnInit() {
-    let submitBtn = document.querySelector('.submitBtn')
     this.questionsData = JSON.parse(localStorage.getItem('questions')!);
-    this.questions = this.questionsData[this.i]
-    
-    let options = document.querySelectorAll('.option')
-    options.forEach(function(button){
-      button.addEventListener('click', function (e: any) {
-        submitBtn?.removeAttribute('disabled');
+    this.questions = this.questionsData[this.i];
+
+    let options = document.querySelectorAll('.option');
+    options.forEach((button) => {
+      button.addEventListener('click', (e: any) => {
+        this.submitBtn?.removeAttribute('disabled');
         options.forEach(function (btn) {
           btn.classList.remove('active');
         });
 
-        
         let selectedAnswer = e.target.parentNode;
-        console.log(selectedAnswer)
+        console.log(selectedAnswer);
         selectedAnswer.classList.add('active');
+      });
+    });
 
-    })
-    })
-
-    submitBtn?.addEventListener('click', ()=> {
-      if(submitBtn){
-        if(submitBtn?.innerHTML=="Submit Answer"){
+    this.submitBtn?.addEventListener('click', () => {
+      if (this.submitBtn) {
+        if (this.submitBtn?.innerHTML == 'Submit Answer') {
           //check if the selected answer is wrong or right
           let selectedAnswer = document.querySelector('.active');
-          let correctAnswerIndex;
-          for (let j = 0; j < this.questions.options.length; j++) {
-            if (this.questions.answer == this.questions.options[j]) {
-              correctAnswerIndex = j;
-            }
-          }
 
           if (
-            correctAnswerIndex &&
+            this.correctAnswerIndex &&
             selectedAnswer?.children[1].innerHTML ==
-              this.questions.options[correctAnswerIndex!]
+              this.questions.options[this.correctAnswerIndex!]
           ) {
             selectedAnswer?.classList.add('correct');
-            this.score+=1
+            this.score += 1;
           } else {
             selectedAnswer?.classList.add('wrong');
-            if (correctAnswerIndex) {
+            if (this.correctAnswerIndex) {
               document
                 .querySelector('.options')
-                ?.children[correctAnswerIndex].classList.add('correct');
+                ?.children[this.correctAnswerIndex].classList.add('correct');
             }
           }
 
-
-          submitBtn.innerHTML = "Next Question"
-        }else {
-          if(this.i<9){
-            submitBtn.innerHTML = "Submit Answer"
-            this.i+=1;
-            this.questions = this.questionsData[this.i]
+          this.submitBtn.innerHTML = 'Next Question';
+        } else {
+          if (this.i < 9) {
+            this.submitBtn.innerHTML = 'Submit Answer';
+            this.i += 1;
+            this.questions = this.questionsData[this.i];
             options.forEach(function (button) {
               options.forEach(function (btn) {
                 btn.classList.remove('active');
@@ -80,28 +74,46 @@ export class QuizComponent {
                 btn.classList.remove('wrong');
               });
             });
-          }else if (this.i == 9){
-            this.router.navigate(['score'])
-          }
-          else if(this.i==8){
-          submitBtn.innerHTML = "Submit Quiz"
-
+          } else if (this.i == 9) {
+            this.router.navigate(['score']);
+          } else if (this.i == 8) {
+            this.submitBtn.innerHTML = 'Submit Quiz';
           }
         }
       }
-      
-    })
+    });
   }
 
-  nextQuestion(index:any){
-    if(this.i<9){
-      this.i+=1;
-      this.questions = this.questionsData[this.i]
-      console.log(this.i)
-    }else if (this.i == 9){
-      this.router.navigate(['score'])
+  nextQuestion(index: any) {
+    if (this.i < 9) {
+      this.i += 1;
+      this.questions = this.questionsData[this.i];
+      console.log(this.i);
+    } else if (this.i == 9) {
+      this.router.navigate(['score']);
     }
-   
+  }
 
+  checkAnswer() {}
+  increaseScore(score: any) {
+    score += 1;
+  }
+
+  getCorrectAnswer() {
+    for (let j = 0; j < this.questions.options.length; j++) {
+      if (this.questions.answer == this.questions.options[j]) {
+        this.correctAnswerIndex = j;
+      }
+    }
+  }
+
+  restartQuiz() {
+    this.options.forEach((button) => {
+      this.options.forEach(function (btn) {
+        btn.classList.remove('active');
+        btn.classList.remove('correct');
+        btn.classList.remove('wrong');
+      });
+    });
   }
 }
